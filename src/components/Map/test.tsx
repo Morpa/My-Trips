dccaaf650821c1a2009b45e288cf2022ef9f442e
Morpa <andremorpa@gmail.com>
2021-08-16 11:16:22 +0100

    Make the Map more dynamic with TDD

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -3,11 +3,40 @@ import { render, screen } from '@testing-library/react'
 import Map from '.'
 
 describe('<Map />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Map />)
+  it('should render without any marker', () => {
+    render(<Map />)
 
-    expect(screen.getByRole('heading', { name: /Map/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Porto',
+      slug: 'porto',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    const placeTwo = {
+      id: '2',
+      name: 'Lisboa',
+      slug: 'lisboa',
+      location: {
+        latitude: 122,
+        longitude: -13
+      }
+    }
+
+    render(<Map places={[place, placeTwo]} />)
+
+    expect(screen.getByTitle(/porto/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/lisboa/i)).toBeInTheDocument()
   })
 })
