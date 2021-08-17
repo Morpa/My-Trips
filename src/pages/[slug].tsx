358b09057001d8a6c0613abc42a1b1e65a46e81e
Morpa <andremorpa@gmail.com>
2021-08-17 20:52:06 +0100

    Add GraphQL Codegen

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,18 +1,20 @@
+import { GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import client from 'graphql/client'
-import { GET_PAGES } from 'graphql/queries'
-import PageTemplate from 'templates/Pages'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
 
-export default function AboutPage() {
+export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
 
   if (router.isFallback) return null
 
-  return <PageTemplate />
+  return <PageTemplate heading={heading} body={body} />
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -21,15 +23,20 @@ export async function getStaticPaths() {
   return { paths, fallback: true }
 }
 
-/* export const getStaticProps = async () => {
-  const { pages } = await client.request(GET_PAGES)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
 
-  console.log(pages)
+  if (!page) return { notFound: true }
 
   return {
-    props: {}
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
   }
-} */
+}
 
 // getStaticPaths => serve para gerar as urls em build time /about, /trip/petropolis
 // getStaticProps => serve para buscar dados da página (props) - build time - estático
--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -3,21 +3,21 @@ import LinkWrapper from 'components/LinkWrapper'
 
 import * as S from './styles'
 
-const PageTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} />
     </LinkWrapper>
 
-    <S.Heading>My Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
 
     <S.Body>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-        voluptatem corrupti est iusto accusantium excepturi deleniti a libero
-        quaerat quae reiciendis dolorum ipsam mollitia error debitis nisi,
-        dolorem quasi reprehenderit.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
