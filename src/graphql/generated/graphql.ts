7ab5ed6b00df5682a7b1442f61923224c3ed921f
Morpa <andremorpa@gmail.com>
2021-08-18 09:44:59 +0100

    Add routes to markers on map

--- src/graphql/generated/graphql.ts
+++ src/graphql/generated/graphql.ts
@@ -2892,7 +2892,16 @@ export type GetPageBySlugQueryVariables = Exact<{
 
 export type GetPageBySlugQuery = { __typename?: 'Query', page?: Maybe<{ __typename?: 'Page', id: string, slug: string, heading: string, body: { __typename?: 'RichText', html: string } }> };
 
-export type GetPlacesQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetPlacesQueryVariables = Exact<{
+  first?: Maybe<Scalars['Int']>;
+}>;
 
 
 export type GetPlacesQuery = { __typename?: 'Query', places: Array<{ __typename?: 'Place', id: string, slug: string, name: string, location: { __typename?: 'Location', latitude: number, longitude: number }, description?: Maybe<{ __typename?: 'RichText', html: string }>, gallery: Array<{ __typename?: 'Asset', url: string, height?: Maybe<number>, width?: Maybe<number> }> }> };
+
+export type GetPlaceBySlugQueryVariables = Exact<{
+  slug: Scalars['String'];
+}>;
+
+
+export type GetPlaceBySlugQuery = { __typename?: 'Query', place?: Maybe<{ __typename?: 'Place', id: string, slug: string, name: string, location: { __typename?: 'Location', latitude: number, longitude: number }, description?: Maybe<{ __typename?: 'RichText', html: string }>, gallery: Array<{ __typename?: 'Asset', url: string, height?: Maybe<number>, width?: Maybe<number> }> }> };
